949a97e0567f9e3531da225e418cb82df6e587ad
madou9 <issoufou.mamadou@gmail.com>
2025-05-11 12:14:10 +0200

    implemented to run the server

---
+++ main.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"context"
+	"log"
+	"net/http"
+	"os"
+	"projectx/usecase"
+
+	"github.com/gorilla/mux"
+	"github.com/joho/godotenv"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/mongo/readpref"
+)
+
+var mongoClient *mongo.Client
+
+func init(){
+	// load .env file
+
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("env load error", err)
+	}
+	log.Println("env file loaded")
+
+	mongoClient, err = mongo.Connect(context.Background(), options.Client().ApplyURI(os.Getenv("MONGO_URI")))
+	if err != nil {
+		log.Fatal("connection error", err)
+	}
+	err = mongoClient.Ping(context.Background(), readpref.Primary())
+	if err != nil {
+		log.Fatal("ping failed", err)
+	}
+	log.Println("mongo connected")
+}
+
+func main() {
+	// close mongo connection
+
+	defer mongoClient.Disconnect(context.Background())
+
+	coll := mongoClient.Database(os.Getenv("DB_NAME")).Collection(os.Getenv("COLLECTION_NAME"))
+
+	// create employee service
+	empService := usecase.EmployeeService{MongoCollection: coll}
+
+	r := mux.NewRouter()
+
+	r.HandleFunc("/health", healthHandler).Methods(http.MethodGet)
+	r.HandleFunc("/employee", empService.CreateEmployee).Methods(http.MethodPost)
+	r.HandleFunc("/employee/{id}", empService.GetEmployeeByID).Methods(http.MethodPut)
+	r.HandleFunc("/employee", empService.GetAllEmployee).Methods(http.MethodGet)
+	r.HandleFunc("/employee/{id}", empService.DeleteEmployeeByID).Methods(http.MethodDelete)
+	r.HandleFunc("/employee", empService.DeleteAllEmployee).Methods(http.MethodDelete)
+	
+	log.Println("server is running on 8080")
+	http.ListenAndServe(":8080", r)
+}
+
+
+func healthHandler(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("running..."))
+}
